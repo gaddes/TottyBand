2d3f6ec9ad97ea2639fa0577352224a6c21eecb2
Matthew Gaddes <matthew.gaddes@gmail.com>
2018-09-23 18:20:45 +0100

    Refactor bootstrap imports and add SASS files.

--- resources/myScript.js
+++
@@ -1,9 +0,0 @@
-function showhideButton() {
-  if (document.getElementById('button_1').innerHTML == "Click here to see more!") {
-    document.getElementById('button_1').innerHTML = "Click here to see less!";
-    document.getElementById('showhide').style.display = 'block';
-  } else {
-    document.getElementById('button_1').innerHTML = "Click here to see more!"
-    document.getElementById('showhide').style.display = 'none';
-  }
-}
